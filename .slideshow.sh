a4624643c7de23be12d1bff361999bd51744895b
Doc <62248488+DocRountree@users.noreply.github.com>
2025-01-11 20:16:37 -0600

    Delete .slideshow.sh

--- .slideshow.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-while true; do
-    # Randomly pick an image from the folder
-    feh --bg-fill --randomize --recursive ~/Pictures
-    # Wait for a specified interval (e.g., 10 seconds)
-    sleep 30
-done
